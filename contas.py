0df63e3ee39214fc41af827a9c0609a69075c3ce
ygormcs <ygormcs@icloud.com>
2024-07-13 13:08:32 -0300

    novos arquivos

--- contas.py
+++ contas.py
@@ -1,3 +1,6 @@
+import datetime
+from extrato import Extrato
+
 class Conta: #chamada da classe >Conta< onde futuramente serao criados os objetos
 
 #criacao dos metodos da classe
@@ -5,13 +8,17 @@ class Conta: #chamada da classe >Conta< onde futuramente serao criados os objeto
     self.clientes
     self.numero
     self.saldo
+    self.sata_abertura = datetime.satatime.today()
+    self.extrato = Extrato()
   def depositar(self, valor): #criacao da funcao de depositar
     self.saldo += valor
+    self.extrato.transacoes.append(["DEPOSITO", valor, "Data", datetime.datetime.today()])
   def sacar(self, valor): #criacao da funcao sacar
     if self.saldo < valor:
       return False
     else:
       self.saldo -= valor
+      self.extrato.transacoes.append(["SACAR", valor, "Data", datetime.datetime.today()])
       return True
   def transfValores(self, cDestino, valor): #criacao da funcao de transf.
       if self.saldo < valor:
@@ -19,6 +26,7 @@ class Conta: #chamada da classe >Conta< onde futuramente serao criados os objeto
       else:
         cDestino.depositar(valor)
         self.saldo -= valor
+        self.extrato.transacoes.append(["TRANSFERENCIA", valor, "Data", datetime.datetime.today()])
         return("Tranferencia realizada")
   def gerarSaldo(self): #criacao da funcao de mostrar saldo
     print(f'numero:{self.numero}\n saldo:{self.saldo}')
--- testecontas.py
+++ testecontas.py
@@ -1,2 +1,18 @@
 from contas import Conta
 from cliente import Cliente
+
+cliente1 = Cliente(12345678910, "Ygor", "Rua Victoria's Secret, 321")
+cliente2 = Cliente(98765432110, "Vini", "Rua Djonga Livre, 3847")
+cliente3 = Cliente(76509823490, "Tulio", "Rua Billabong Pato, 456")
+cliente4 = Cliente(23409745678,"Edson", "Rua Lanternas dos Afogados, 678")
+
+conta1 = Conta([cliente1, cliente2, cliente3, cliente4], 1, 0)
+
+conta1.gerarSaldo()
+conta1.depositar(1500)
+conta1.sacar(500)
+conta1.gerarSaldo()
+
+for clientes in conta1.clientes:
+    print(f'O cliente se chama {cliente.nome}, cujo CPF e {cliente.cpf} e reside em {cliente.endereco}')
+
